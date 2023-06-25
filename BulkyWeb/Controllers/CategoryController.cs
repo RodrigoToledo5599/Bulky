3a9b1230fcff196c042595a737074dd209a0d508
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-06-25 06:32:05 -0300

    Create Page And Methods DONE

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -8,31 +8,51 @@ namespace BulkyWeb.Controllers
     {
         public AppDbContext _db { get;set; }
         public List<Category> categories { get; set; }
-
+        public Category Category { get; set; }
 
         public CategoryController(AppDbContext db) 
         {
             _db = db;
         }
-
+        #region Index
         public IActionResult Index()
         {
             categories = _db.Category.ToList();
             return View(categories);
         }
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    }
+        #endregion
+
+        #region Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+                return View(); 
+        }
+
+		#endregion
+
+		#region Delete
+
+
+
+
+
+
+
+
+	}
 }
