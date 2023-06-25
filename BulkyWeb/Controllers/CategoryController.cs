2e150daa7f7b284417391bee159fa53c4c63c076
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-06-25 06:11:17 -0300

    Pagina de lista de categorias completa

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        public AppDbContext _db { get;set; }
+        public List<Category> categories { get; set; }
+
+
+        public CategoryController(AppDbContext db) 
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            categories = _db.Category.ToList();
+            return View(categories);
         }
     
+
     
     
     
